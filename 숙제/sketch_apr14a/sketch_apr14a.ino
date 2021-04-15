bb757365a0a3f94afa7dc4b375bc371728869e9e
diadntjr <dntjr0501@gmail.com>
2021-04-15 16:19:10 +0900

    3-7 문제
    1차 수정

--- 숙제/sketch_apr14a/sketch_apr14a.ino
+++ 숙제/sketch_apr14a/sketch_apr14a.ino
@@ -1,11 +1,38 @@
-byte Pin[] = {2,3,4,5,6,7,8};
-
-
+#include <stdio.h>
+byte Pin[] = {2,3,4,5};
+byte Pin_num = sizeof(Pin) / sizeof(byte);
+byte sendNumber = 0;
+byte count = 0;
+byte Pin_ON[] = {0,0,0,0};
 
 void setup() {
-    
+  for(int j = 0; j<Pin_num; j++) {
+    pinMode(Pin[j], OUTPUT); 
+  }
+}
+
+int function(int receiveNumber) {
+  for (int i = 3; i >= 0; i--) { //8자리 숫자까지 나타냄
+        Pin_ON[i] = receiveNumber >> i;
+    }
+  return 0;
 }
 
 void loop() {
-
+  function(sendNumber);
+  if(sendNumber == 16) {
+    sendNumber = 0;
+  }
+  for(int i = 0; i<=4; i++) {
+    if(Pin_ON[i] == 1) {
+      digitalWrite(i+2, HIGH);
+      delay(500);
+      Pin_ON[i] = 0;
+    }
+    else {
+      digitalWrite(i+2, LOW);
+      delay(500);
+    }
+  }
+  sendNumber++;
 }
