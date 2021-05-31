862ba5a9da95a75955cae506d8b9a954da420e11
diadntjr <dntjr0501@gmail.com>
2021-05-31 13:47:43 +0900

    숙제 추가

---
+++ 숙제/1316_4-3/sketch_may31a/sketch_may31a.ino
@@ -0,0 +1,25 @@
+int pin[] = {2,3,4,5,6,7,8,9};
+
+void setup()
+{
+  Serial.begin(9600);
+  for(int i = 2; i!=10; i++)  {
+   pinMode(pin[i],OUTPUT);
+  }
+}
+
+void loop()
+{
+  if(Serial.available()){
+   int value1 = Serial.parseInt();
+   int value2;
+    value2 = ~value1;
+   if(digitalRead(pin[value1]-1)==HIGH) {
+      digitalWrite(pin[value1]-1,~(value1^value2));
+    }
+    else {
+      digitalWrite(pin[value1]-1,value1);
+    }
+    
+  }
+}
